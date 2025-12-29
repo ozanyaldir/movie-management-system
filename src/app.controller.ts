fa9192d463b933d8cce08a625b55bf0af356917d
Ozan Yaldır <8179156+ozanyaldir@users.noreply.github.com>
2025-12-29 17:05:56 +0300

    added readiness endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, ServiceUnavailableException } from '@nestjs/common';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Controller()
 export class AppController {
-  constructor() {}
+  constructor(@InjectDataSource() private readonly dataSource: DataSource) {}
 
   @Get('/healthcheck')
   healthcheck(): string {
     return 'UP';
+  }
+
+  @Get('/readiness')
+  async readiness() {
+    try {
+      await this.dataSource.query('SELECT 1');
+      return { status: 'READY' };
+    } catch {
+      throw new ServiceUnavailableException({
+        status: 'NOT_READY',
+        reason: 'Database unavailable',
+      });
+    }
   }
 }
