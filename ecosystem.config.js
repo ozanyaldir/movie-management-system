cee085eb7ee7df76e19026dd1a87f9bae4d00773
Ozan Yaldır <8179156+ozanyaldir@users.noreply.github.com>
2025-12-30 22:27:39 +0300

    Added pm2 config and root directory reference

---
+++ ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apps : [{
+    name   : "movie-management-system",
+    script : "npm start",
+    error_file: "./logs/error.log",
+    log_date_format: "YYYY-MM-DD HH:mm Z",
+  }]
+}
